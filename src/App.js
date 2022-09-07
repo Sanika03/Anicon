24a4cad45cdef29c287785cc207f9a04cc3a4b1b
Sanika Suryawanshi <sanikasuryawanshi0305@gmail.com>
2022-09-07 04:21:48 +0000

    Added processing and output

--- src/App.js
+++ src/App.js
@@ -14,14 +14,54 @@ var emojiDictionary = {
   "🦌": "Deer"
 };
 
+var emojisWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
+  
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We do not have this value in our database";
+    }
+
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  
   return (
     <div className="App">
+      
       <h1>Anicon</h1>
+      
       <input
         placeholder="Type your emoji here"
         onChange={emojiInputHandler}
       ></input>
+      
+      <h2>{meaning}</h2>
+      
+      <h3>Emojis we know</h3>
+      
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
